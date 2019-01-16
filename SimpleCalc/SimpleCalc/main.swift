ac80b4e5e69daca1e2002c549f6d6de1a313c0b2
yan530 <yan530@uw.edu>
2019-01-15 19:10:03 -0800

    done

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,63 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        var result = 0
+        var index = 0
+        let size = args.count
+        let last = args[size - 1]
+        switch last {
+            case "count":
+                if size == 1 {
+                    result = 0
+                } else {
+                    return size - 1
+                }
+            case "avg":
+                while index < size {
+                    result += Int(args[index]) ?? 0
+                    index += 1
+                }
+                if size == 1 {
+                    result = 0
+                } else {
+                    result = result / (size - 1) 
+                }
+            case "fact":
+                if size == 1 {
+                    result = 0
+                } else {
+                    result = Int(args[0]) ?? 0
+                    var num = (Int(args[0]) ?? 0) - 1
+                    while num > 1 {
+                        result *= num
+                        num -= 1
+                    }
+                }
+            default :
+                result = Int(args[0]) ?? 0
+                index = 1
+                while index < size {
+                    switch args[index] {
+                        case "+":
+                            result += Int(args[index + 1]) ?? 0
+                        case "-":
+                            result -= Int(args[index + 1]) ?? 0
+                        case "*":
+                            result *= Int(args[index + 1]) ?? 0
+                        case "/":
+                            result /= Int(args[index + 1]) ?? 1
+                        case "%":
+                            let num = Int(args[index + 1]) ?? 1
+                            let divide = result / num
+                            result = result - num * divide
+                        default :
+                            result = result * 1
+                    }
+                    index += 2
+            }
+            
+        }
+        return result
     }
     
     public func calculate(_ arg: String) -> Int {
